d3a6a496c06e0a69ce091e259f09da011893e4f6
Mohit Kumar Kokcha <130283675+mohitkokcha203@users.noreply.github.com>
2024-09-27 21:29:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Header from './components/Header';
+import Header from './Components/Header';
 import DatePicker from './components/DatePicker';
 import RecurrenceOptions from './components/RecurrenceOptions';
 import PreviewCalendar from './components/PreviewCalendar';
